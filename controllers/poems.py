8b1dc5870669ef4638c401b457bca30976fa1867
Peter Christensen <tessercat@pakaa.net>
2017-07-19 12:18:43 -0600

    Added poems and poem thumb pages to ram cache.

--- controllers/poems.py
+++ controllers/poems.py
@@ -13,14 +13,16 @@ def index():
         logger.error('Bad index: %s, %s', request.args, request.vars)
         raise HTTP(404)
     try:
-        idx = poems.index(0, _page_size, db)
-        pager = poems.pager(_page_size, db)
+        idx = cache.ram(
+            'poems-1',
+            lambda: poems.index(0, _page_size, db))
+        pager = cache.ram(
+            'poems_pager-1',
+            lambda: poems.pager(_page_size, db))
     except:
         logger.exception('Bad index: %s', request.args(0))
         raise HTTP(404)
-    return {
-        'index': idx,
-        'pager': pager}
+    return {'index': idx, 'pager': pager}
 
 def chapter():
     try:
@@ -37,20 +39,21 @@ def chapter():
     except:
         logger.exception('Bad chapter: %s', request.args(0))
         raise HTTP(404)
-    return {
-        'poem': poem,
-        'links': links}
+    return {'poem': poem, 'links': links}
 
 def page():
     try:
         response.title = '道德經 Page %s' % request.args(0)
-        xml = poems.index(int(request.args(0)), _page_size, db)
+        idx = cache.ram(
+            'poems-%s' % request.args(0),
+            poems.index(int(request.args(0)), _page_size, db))
+        pager = cache.ram(
+            'poem_pager-%s' % request.args(0),
+            poems.pager(_page_size, db))
     except:
         logger.exception('Bad page: %s', request.args(0))
         raise HTTP(404)
-    return {
-        'index': xml,
-        'pager': poems.pager(_page_size, db)}
+    return {'index': idx, 'pager': pager}
 
 @auth.requires_login()
 def manage():
