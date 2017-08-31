fd502fe23ae2ae57601a9da6adaf8030b98c5372
Peter Christensen <tessercat@pakaa.net>
2017-08-31 01:08:55 -0600

    Improved about page.

--- controllers/about.py
+++ controllers/about.py
@@ -6,22 +6,21 @@ def index():
     """ Return a dict for the about view. """
 
     def about_page():
-
         md_dir =  '/opt/ddj/book/markdown'
-        logger.debug('Generating new about page')
+        page = {}
         with open(os.path.join(md_dir, 'about.md')) as fd:
-            about = markdown2.markdown(fd.read())
-        with open(os.path.join(md_dir, 'copyright.md')) as fd:
-            copyright = markdown2.markdown(fd.read())
+            page['about'] = markdown2.markdown(fd.read())
         with open(os.path.join(md_dir, 'apology.md')) as fd:
-            apology = markdown2.markdown(fd.read())
+            page['apology'] = markdown2.markdown(fd.read())
+        with open(os.path.join(md_dir, 'appresources.md')) as fd:
+            page['resources'] = markdown2.markdown(fd.read())
+        with open(os.path.join(md_dir, 'copyright.md')) as fd:
+            page['copyright'] = markdown2.markdown(fd.read())
         with open(os.path.join(md_dir, 'references.md')) as fd:
-            references = markdown2.markdown(fd.read())
-        return {
-            'about': about,
-            'copyright': copyright,
-            'apology': apology,
-            'references': references}
+            page['references'] = markdown2.markdown(fd.read())
+        with open(os.path.join(md_dir, 'saints.md')) as fd:
+            page['saints'] = markdown2.markdown(fd.read())
+        return page
 
     response.title = 'About the 道德經'
     return cache.ram('about', lambda: about_page())
